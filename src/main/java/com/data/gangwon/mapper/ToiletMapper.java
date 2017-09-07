738d73f650f82c366c419ba68a98e30243a1dd9d
whdms705 <whdms705@nate.com>
2017-09-08 02:33:14 +0900

    update build.gradle

--- src/main/java/com/data/gangwon/mapper/ToiletMapper.java
+++ src/main/java/com/data/gangwon/mapper/ToiletMapper.java
@@ -1,17 +1,14 @@
 package com.data.gangwon.mapper;
 
 import java.util.List;
-
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
-
 import com.data.gangwon.dto.ToiletDTO;
 
 
 @Mapper
 public interface ToiletMapper {
 
-	 @Select("select count(*) from TOILET_TB;")
+	 
 	 public Integer toiletCount();
 	    
 
@@ -19,7 +16,7 @@ public interface ToiletMapper {
 	    
 	 //LateDiseaseDTO lateDiseaseDetail(int id);
 	        
-	 //2�� �̻��� �Ķ���͸� �ѱ���� @Param���� ���� or �װ� ������ map���� �Ѱܶ�
+	 //2占쏙옙 占싱삼옙占쏙옙 占식띰옙占쏙옙拷占� 占싼깍옙占쏙옙占� @Param占쏙옙占쏙옙 占쏙옙占쏙옙 or 占쌓곤옙 占쏙옙占쏙옙占쏙옙 map占쏙옙占쏙옙 占싼겨띰옙
 	 //String[] autocomplete(@Param("type") int type, @Param("text") String text, @Param("category") String category);
 	    
 	
